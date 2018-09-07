53b6ac884a3856b22aebd87b1b6a311ff304cd15
Jonathan Vogt <jonathan.vogt@allcloud.io>
2018-09-07 17:25:46 +0200

    dev

--- clisso.rb
+++ clisso.rb
@@ -9,7 +9,7 @@ class Clisso < Formula
   depends_on "dep" => :build
 
   bottle do
-    root_url 'https://github.com/allcloud-jonathan/clisso/releases/download/' + version
+    root_url 'https://github.com/allcloud-jonathan/clisso/releases/download/0.5.0'
     sha256 "e873ecc13c57da29b1edd793c4283ad675a20b8b28e5ff9cab1c853dd38eebde" => :high_sierra
   end
 
