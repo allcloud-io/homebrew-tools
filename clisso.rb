f4995685339e3c4fe96329c0de1538e50caed39d
Jonathan Vogt <jonathan.vogt@allcloud.io>
2021-03-08 22:46:41 +0100

    Automatic commit of bottle build for version 0.8.3 of clisso.

--- clisso.rb
+++ clisso.rb
@@ -1,13 +1,13 @@
 class Clisso < Formula
   desc "Get temporary credentials for cloud providers from the command-line"
   homepage "https://github.com/allcloud-io/clisso"
-  url "https://github.com/allcloud-io/clisso/archive/0.8.2.tar.gz"
-  sha256 "226db7d930aeaf4b0279169f223798063d3cd07b8610e59d08cc3ef28235da43"
+  url "https://github.com/allcloud-io/clisso/archive/0.8.3.tar.gz"
+  sha256 "32a425eed2e98c6586230864ffe15086fae7c4bbdb20d7f8ae6cebd692951fe8"
 
   bottle do
-    root_url "https://github.com/allcloud-io/clisso/releases/download/0.8.2"
+    root_url "https://github.com/allcloud-io/clisso/releases/download/0.8.3"
     cellar :any_skip_relocation
-    sha256 "c5f312c17e0e2272b707dadd6bf99dba075645bbd3854cae7a506427c02e02ce" => :big_sur
+    sha256 "9b08734bb31d0cb5533724fcfb89ee13952a7dc93639be7c039cc79c94e04070" => :big_sur
   end
 
   depends_on "go" => :build
@@ -20,8 +20,9 @@ class Clisso < Formula
     dir.install buildpath.children - [buildpath/".brew_home"]
     cd dir do
       ENV["VERSION"] = version
-      system "make", "-e"
-      bin.install "build/clisso" => "clisso"
+      system "make", "-e", "unsigned-darwin-amd64-zip"
+      system "unzip assets/clisso-darwin-amd64.zip"
+      bin.install "clisso-darwin-amd64" => "clisso"
     end
   end
 
